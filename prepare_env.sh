73fc46e3a00775be50f58167f31b1d8ce52cab64
VanLinLin <aas12as12as12tw@gmail.com>
2024-08-12 12:07:18 +0800

    fix some bugs

--- prepare_env.sh
+++ prepare_env.sh
@@ -12,6 +12,8 @@ conda activate hsic_env
 
 conda install pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda=11.7 -c pytorch -c nvidia -y
 
-conda install matplotlib pandas lightgbm scikit-learn 
+conda install matplotlib pandas lightgbm scikit-learn imageio scikit-image -y
+
+conda install -n hsic_env ipykernel --update-deps --force-reinstall -y
 
 echo Finish creating environment!
